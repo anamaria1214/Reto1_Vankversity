a4460f456a73021b8b1f2dc8d2ffa33696008e09
Ana María <anicu2314@gmail.com>
2024-11-23 15:05:27 -0500

    Mensaje principal y menú

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("Bienvenido a EcoTracker: Aprende sobre el cambio climático y cómo reducir tu impacto en el medio ambiente.");
+
+let opcion=prompt("Ingrese una opción: \n 1. Introducción al Cambio Climático. \n 2. Huella de Carbono Personal. \n 3. Consejos Prácticos para Reducir tu Impacto");
